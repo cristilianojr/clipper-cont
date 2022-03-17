8cd47d92cf82ffdcac491ef81f1e04a7dc35a459
Cristiliano Júnior <cristilianojunior@gmail.com>
2022-03-17 02:43:41 -0300

    v1.015

--- scripts/email.php
+++ scripts/email.php
@@ -8,11 +8,11 @@
         
         $to = "contato@clippercont.com";
         $subjet = "Contato - Clipper Contabilidade";
-        $body = "Nome: ".$name."\n".
-                "Email: ".$email."\n".
+        $body = "Nome: ".$name."\r\n".
+                "Email: ".$email."\r\n".
                 "Mensagem: ".$message;
         $header = "From:".$email."\r\n".
-                    "Reply-To".$email."\e\n".
+                    "Reply-To:".$email."\r\n".
                     "X=Mailer:PHP/".phpversion();
         
         mail($to, $subjet, $body, $header);
